1870a4840a715fac92ec3a12304c6feeaf423476
Lauren Reilly <LaurenReillyNet@gmail.com>
2018-11-01 20:11:57 -0400

    completed exercise one, generating adorable pups

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var dogContainer = document.getElementById("dog");
 var button = document.getElementById("generate");
 
-button.addEventListener("click", generateDog());
+button.addEventListener("click", generateDog);
 
 function generateDog() {
     button.innerHTML = "Generating Pup...";
-    axios.get('https://dog.ceo/api/breeds/image/random');
+    axios.get("https://dog.ceo/api/breeds/image/random").then(function(data) {
+        dogContainer.innerHTML = `<img src="${data.data.message}" style="height: 300px; width: auto">`
+        button.innerHTML = "Generate Pup";    
+    });
 }
 
